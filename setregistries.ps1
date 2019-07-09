e41c82ee7203a20050913b41ececd6ed7499b23c
pasha0077 <pasha.spr0077@gmail.com>
2019-07-09 22:00:18 +0530

    added more things. Note to self: Commit and push asap after modifications

--- setregistries.ps1
+++ setregistries.ps1
@@ -1,23 +1,39 @@
 #Below cmdlet is to make the ps1 run
 #Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
-Function SetRegistry {
-    Param($enable,$setting,$path,$keyname,$keytype,$keyvalue)
+Function ModifyRegistry {
+    Param($enable,$action,$setting,$path,$keyname,$keytype,$keyvalue)
     If (($enable.ToUpper() -eq "YES") -OR ($enable.ToUpper() -eq "TRUE")) {
         Write-Host "Setting :"$setting
-        If (!(Test-Path $path)) {
-		    New-Item -Path $path -Force | Out-Null
-	    }
-	    Set-ItemProperty -Path $path -Name $keyname -Type $keytype -Value $keyvalue
+        $path,$keyname,$keytype,$keyvalue
+        
+        If($action.ToUpper() -eq "DELETEPATH")
+        {
+            If ((Test-Path $path)) {
+                Remove-Item -Path $path -Recurse 
+	        }
+        }
+        ElseIf($action.ToUpper() -eq "DELETEKEY") {
+            If ((Test-Path $path)) {
+                Remove-ItemProperty -Path $path -Name $keyname 
+	        }
+        }
+        Else
+        {
+            If (!(Test-Path $path)) {
+		        New-Item -Path '$path' -Force | Out-Null
+	        }
+	        Set-ItemProperty -Path $path -Name $keyname -Type $keytype -Value $keyvalue
+        }
 	}
     Else
     {
-        Write-Host "Skipping :"$setting
+        Write-Host "Skipping setting: "$setting
     }
 }
 
-[xml]$xml=get-content $PSScriptRoot\registries.xml
+[xml]$xml=get-content $PSScriptRoot\test.xml
 $nodes = select-xml "//registry" $xml
 $nodes|foreach-object{
-    SetRegistry -setting $_.node.name -enable $_.node.enable -path $_.node.path -keyname $_.node.keyname -keytype $_.node.keytype -keyvalue $_.node.keyvalue
-}
+    ModifyRegistry -setting $_.node.name -action $_.node.action -enable $_.node.enable -path $_.node.path -keyname $_.node.keyname -keytype $_.node.keytype -keyvalue $_.node.keyvalue
+} 

b77e7c8401098709a7ae9b3e339a5806c2bc78e4
pasha0077 <pasha.spr0077@gmail.com>
2019-08-11 01:51:00 +0530

    Added SelfSigning feature "Half-Baked"

---
+++ selfsigningremove.ps1
@@ -0,0 +1,36 @@
+﻿$pfx = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 
+$pass = ConvertTo-SecureString -String "password" -Force -AsPlainText
+$certPat = "c:\SelfCert.pfx"
+$pfx.import($certPat,$pass,"Exportable,PersistKeySet") 
+#$pfx.import($cert.RawData,$pass,"Exportable,PersistKeySet") 
+
+$store = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::Root,
+    "localmachine"
+)
+$store.open("MaxAllowed") 
+$store.remove($pfx) 
+$store.close()
+$store = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::TrustedPublisher,
+    "localmachine"
+)
+$store.open("MaxAllowed") 
+$store.remove($pfx) 
+$store.close()
+$store = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::My,
+    "currentuser"
+)
+$store.open("MaxAllowed") 
+$store.remove($pfx) 
+$store.close()
+$store = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::CertificateAuthority,
+    "currentuser"
+)
+$store.open("MaxAllowed") 
+$store.remove($pfx) 
+$store.close()
+
+del $certPat
---
+++ selfsigningwithcerts.ps1
@@ -0,0 +1,30 @@
+﻿$cert = New-SelfSignedCertificate -CertStoreLocation Cert:\CurrentUser\My -DnsName Myself -Type CodeSigningCert
+$pass = ConvertTo-SecureString -String "password" -Force -AsPlainText
+$CertPath = "Cert:\CurrentUser\My\$($cert.Thumbprint)"
+$CurrFolder = read-host "Folder Path to files";
+Export-PfxCertificate  -Cert $CertPath -FilePath c:\SelfCert.pfx -Password $pass
+
+$pfx = new-object System.Security.Cryptography.X509Certificates.X509Certificate2 
+$certPat = "c:\SelfCert.pfx"
+$pfx.import($certPat,$pass,"Exportable,PersistKeySet") 
+#$pfx.import($cert.RawData,$pass,"Exportable,PersistKeySet") 
+
+$store = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::Root,
+    "localmachine"
+)
+$store.open("MaxAllowed") 
+$store.add($pfx) 
+$store.close()
+$store2 = new-object System.Security.Cryptography.X509Certificates.X509Store(
+    [System.Security.Cryptography.X509Certificates.StoreName]::TrustedPublisher,
+    "localmachine"
+)
+$store2.open("MaxAllowed") 
+$store2.add($pfx) 
+$store2.close()
+#$cert = Get-ChildItem -Path 'Cert:\CurrentUser\My\A9FA1AFB76E549529F1444179E7139CDCAB55F1B';
+$cert.EnhancedKeyUsageList;
+Set-AuthenticodeSignature -Certificate:$cert -FilePath:$CurrFolder'\setregistries.ps1';
+Set-AuthenticodeSignature -Certificate:$cert -FilePath:$CurrFolder'\permissions.ps1';
+Set-AuthenticodeSignature -Certificate:$cert -FilePath:$CurrFolder'\selfsigningremove.ps1';
